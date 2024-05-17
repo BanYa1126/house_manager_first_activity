d57cbf22f24c5d3b5d4a69578c7c034030c6fb74
JHLgithuber <jfttstudio@gmail.com>
2024-05-18 05:44:49 +0900

    Revert "change xml1"
    This reverts commit 68726a38f48a3aeec7990844dda6d4925085bb86.

--- app/src/main/java/com/example/housemanager/HouseMActivity.java
+++ app/src/main/java/com/example/housemanager/HouseMActivity.java
@@ -15,7 +15,7 @@ public class HouseMActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.claimmoney_activity);
+        setContentView(R.layout.claimmoney_activity_main);
 
         Context context = this;
         // 헤더 레이아웃 포함
