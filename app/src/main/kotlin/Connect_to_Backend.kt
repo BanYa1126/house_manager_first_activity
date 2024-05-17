08100d7b02f353fb0b36639c08770afbfa73acaa
JHLgithuber <jfttstudio@gmail.com>
2024-05-18 05:26:30 +0900

    로그인기능 서버 통신 구현
    개발용 ID, PW는 카톡 참조

--- app/src/main/kotlin/Connect_to_Backend.kt
+++ app/src/main/kotlin/Connect_to_Backend.kt
@@ -3,44 +3,31 @@ package com.example.housemanager;
 import android.util.Log
 import io.socket.client.IO
 import io.socket.client.Socket
-import okhttp3.OkHttpClient
 import org.json.JSONObject
-import java.security.cert.X509Certificate
-import javax.net.ssl.X509TrustManager
-import javax.net.ssl.SSLContext
-import javax.net.ssl.TrustManager
-import javax.net.ssl.HostnameVerifier
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import android.widget.Toast
+import android.content.Context
+
+interface LoginCallback {
+    fun onLoginResult(result: String)
+}
 
 class Connect_to_Backend {
     private lateinit var mSocket: Socket
     private val TAG = "Connect_to_Backend" // 로그를 구분하기 위한 TAG 설정
+    var accessToken: String? = null
+    var refreshToken: String? = null
+    var permission: String? = null
 
     init {
-        try {/*
-            // SSL/TLS 인증서 무시
-            val trustManager = object : X509TrustManager {
-                override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
-                override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
-                override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
-            }
-
-            val sslContext = SSLContext.getInstance("SSL")
-            sslContext.init(null, arrayOf<TrustManager>(trustManager), java.security.SecureRandom())
-
-            val okHttpClient = OkHttpClient.Builder()
-                .sslSocketFactory(sslContext.socketFactory, trustManager)
-                .hostnameVerifier { _, _ -> true }
-                .build()
-
-
-            // IO.Options 인스턴스에 OkHttpClient 설정을 적용
-            val options = IO.Options()
-            options.callFactory = okHttpClient
-            options.webSocketFactory = okHttpClient*/
-
+        try {
 
             // 서버 URL 설정
-            mSocket = IO.socket("https://10.0.2.2:5000")
+            //mSocket = IO.socket("https://10.0.2.2:8000")
+            mSocket = IO.socket("https://hm.jftt.kr") //For Real Docker Server
+            //mSocket = IO.socket("https://10.0.2.2:5000") //For localhost TEST
             try {
                 Log.d(TAG, "서버 연결시도중...")
                 mSocket.connect()
@@ -52,13 +39,13 @@ class Connect_to_Backend {
                         Log.e(TAG, "연결 에러 발생: ", e)
                     }
                 }
-            }catch (e: Exception){
+            } catch (e: Exception) {
                 Log.e(TAG, "연결 실패: ", e)
             }
 
             // 서버로부터 메시지 받기
             mSocket.on("receive_message") { args ->
-                Log.d(TAG, "최초메시지: " + args[0])
+                Log.d(TAG, "메시지: " + args[0])
                 if (args[0] is Boolean) {
                     val success = args[0] as Boolean
                     handleLoginResponse(success)
@@ -70,11 +57,42 @@ class Connect_to_Backend {
         }
     }
 
-    fun login(username: String, password: String) {
+
+    fun login(context: Context, username: String, password: String, callback: LoginCallback) {
         val data = JSONObject()
         data.put("username", username)
         data.put("password", password)
-        mSocket.emit("login", data)
+        //mSocket.emit("login", data)
+        Toast.makeText(context, "Please wait...", Toast.LENGTH_SHORT).show()
+
+        val loginRequest = LoginRequest(username = username, password = password)
+        RetrofitInstance.api.login(loginRequest).enqueue(object : Callback<LoginResponse> {
+            override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
+                if (response.isSuccessful) {
+                    accessToken = response.body()?.access_token
+                    refreshToken = response.body()?.refresh_token
+                    permission = response.body()?.permission
+                    Log.d(
+                        TAG,
+                        "Login successful by $permission, access token: $accessToken, refresh token: $refreshToken"
+                    )
+                    // 토큰을 저장하고 보호된 경로에 접근할 때 사용
+                    callback.onLoginResult("Successful by $permission")
+                } else {
+                    val loginResult = response.message()
+                    callback.onLoginResult(loginResult)
+                    Log.d(TAG, "Login failed: ${loginResult}")
+                    Toast.makeText(context, "Login failed: ${loginResult}", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                val loginResult = t.message
+                callback.onLoginResult(loginResult ?: "Unknown error")
+                Log.e(TAG, "Login error: ${loginResult}")
+                Toast.makeText(context, "Unknown error: ${loginResult}", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
     private fun handleLoginResponse(success: Boolean) {
