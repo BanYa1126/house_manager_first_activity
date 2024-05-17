cf707e79d9326ad33a94f7b9de1dc3ef0c88eac2
JHLgithuber <jfttstudio@gmail.com>
2024-05-18 05:31:01 +0900

    이게 뭐고

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.4.0" apply false
+<<<<<<< Updated upstream
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+=======
+>>>>>>> Stashed changes
 }
