3e270d5481a21665218c7b04cf23337a099bb30d
BanYa1126 <132338040+BanYa1126@users.noreply.github.com>
2024-05-19 21:53:10 +0900

    listVIew test case1
    리스트뷰 테스트.

--- app/src/main/java/com/example/housemanager/RegistrationActivity.java
+++ app/src/main/java/com/example/housemanager/RegistrationActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.widget.AdapterView;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -20,6 +23,27 @@ public class RegistrationActivity extends AppCompatActivity {
 
         HomeIconClickListener listener = new HomeIconClickListener(this);
         headerAdmin.setOnClickListener(listener);
-    }
 
+        ListView listView = findViewById(R.id.person_listview);
+
+        // 표시할 단일 데이터
+        String[] data = {"test1","test2","test3","test4","test5"};
+
+        // ArrayAdapter 생성
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                data
+        );
+        // ListView에 어댑터 설정
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Intent를 사용하여 SecondActivity로 전환
+                Intent intent = new Intent(RegistrationActivity.this, RegistrationActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
