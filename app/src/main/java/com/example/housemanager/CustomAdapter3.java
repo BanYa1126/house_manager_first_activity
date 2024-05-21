fa530c4c1c1c78894c66f237e1223e5344159327
RKskell <147506277+RKskell@users.noreply.github.com>
2024-05-21 19:21:13 +0900

    upload_Mr.choi
    어 나 최영재인데 잘써라.

---
+++ app/src/main/java/com/example/housemanager/CustomAdapter3.java
@@ -0,0 +1,48 @@
+package com.example.housemanager;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class CustomAdapter3 extends BaseAdapter {
+
+    private Context context;
+    private String[] RegNum;
+    private LayoutInflater inflater;
+
+    public CustomAdapter3(Context context, String[] RegNum) {
+        this.context = context;
+        this.RegNum = RegNum;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return RegNum.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return RegNum[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.claimmoney_activity, parent, false);
+        }
+
+        TextView RegNumberText = convertView.findViewById(R.id.RegNumber);
+
+        RegNumberText.setText("세대 번호 : " + RegNum[position]);
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/housemanager/HouseMActivity.java
+++ app/src/main/java/com/example/housemanager/HouseMActivity.java
@@ -8,10 +8,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Toast;
 public class HouseMActivity extends AppCompatActivity {
     private static final String TAG = "HouseMActivity"; // 로그를 구분하기 위한 TAG 설정
@@ -28,13 +30,34 @@ public class HouseMActivity extends AppCompatActivity {
 
         Context context = this;
 
-        // Singleton 인스턴스 가져오기
-        backend = Connect_to_Backend.getInstance();
-        backend.setEventCallback(new EventCallback() {
+        ListView listView = findViewById(R.id.HouseNumber_listview);
+
+        // 표시할 단일 데이터
+        String[] data = {"Test data 101", "Test data 102", "Test data 103", "Test data 104", "Test data 201"};
+
+        CustomAdapter3 adapter = new CustomAdapter3(this, data);
+
+        // ListView에 어댑터 설정
+        listView.setAdapter(adapter);
+
+        // ListView 아이템 클릭 리스너 설정
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onEventReceived(ReceivedDataEvent event) {
-                Log.d(TAG, "Received data: " + event.getMessage());
-                // 받은 데이터의 JSON을 알아서 파싱해서 UI 업데이트 등의 작업 수행
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Intent를 사용하여 SecondActivity로 전환
+                Intent intent = new Intent(HouseMActivity.this, HouseMActivity1.class);
+                startActivity(intent);
+                setContentView(R.layout.claimmoney_activity);
+
+                // Singleton 인스턴스 가져오기
+                backend = Connect_to_Backend.getInstance();
+                backend.setEventCallback(new EventCallback() {
+                    @Override
+                    public void onEventReceived(ReceivedDataEvent event) {
+                        Log.d(TAG, "Received data: " + event.getMessage());
+                        // 받은 데이터의 JSON을 알아서 파싱해서 UI 업데이트 등의 작업 수행
+                    }
+                });
             }
         });
     }
